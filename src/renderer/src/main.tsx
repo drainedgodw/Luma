d3321e78c050131103e237aefa0740245630ba31
upset <drainedgodgit@gmail.com>
2026-09-09 00:14:29 +0600

    feat: add adjustable wallpaper blur

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './theme.css';
 import './performance.css';
+import './wallpaper.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
